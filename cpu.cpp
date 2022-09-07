91eeeb72fbeb99205dea07e6b6d52d8b0f5e4c1e
Matthew Graham <matthewjgraham@tutanota.com>
2022-09-07 17:34:15 -0400

    Add CPU with single instruction AND
    It only supports AND with the immediate memory access mode for now

---
+++ cpu.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "cpu.h"
+
+void CPU::reset(Badge<NESEmulator>)
+{
+    mP = 0x34;
+    mA = mX = mY = 0;
+    mSP = 0xfd;
+    mClockCycle = 0;
+}
+
+void CPU::runOpcode(EncodedInstructionType inst)
+{
+    printf("runOpcode happens with %u\n", (unsigned)inst);
+    
+    switch (inst) {
+	RUN_IF_ALU_OPCODE(AND)
+    }
+}
+
+
+void CPU::AND(MemoryAccessMode mode)
+{
+    // just accumulator mode
+    printf("This happens\n");
+    switch (mode) { // TODO: A large switch statement to check for the access mode will not do down the line. Too much duplication.
+    case MemoryAccessMode::Immediate:
+	mA &= mA;
+	break;
+    default:
+	UNDEFINED_MEMORY_ACCESS_MODE(mode);
+    }
+}
