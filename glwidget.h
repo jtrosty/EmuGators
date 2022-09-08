8ec774ee6b6414e39682d1063dc0e6e254b69244
Trost_linux <j.trosty@gmail.com>
2022-09-08 03:10:29 -0700

    Have window working, you can change the color with the WASD keys.

--- glwidget.h
+++ glwidget.h
@@ -17,20 +17,21 @@ class GLWidget : public QOpenGLWidget
 public:
     // VARIABLES
     uchar* pixelData;
-    QImage* imageData;
-    QPixmap* pixmap;
-    uchar startR = 0x88;
+    uchar startR = 0xFF;
     uchar startB = 0x00;
-    uchar startG = 0xFF;
+    uchar startG = 0x00;
     uchar startA = 0xFF;
 
     int width = 256;
     int height = 240;
     int scale = 2;
     int bytesPerPixel = 4;
+    int widthScaled = width * scale;
+    int heightScaled = height * scale;
+    int bytesPerLine = width * bytesPerPixel;
     uchar inputX = 0;
     uchar inputY = 0;
-    uint pixelDataLength = width * height * scale * bytesPerPixel;
+    uint pixelDataLength = width * height * bytesPerPixel;
 
     bool test_W = false;
 
@@ -41,23 +42,17 @@ public:
     void keyPressEvent(QKeyEvent* event) override;
 
     // Slots
-    void updateImage();
 
     // Signals
-    void PixelDataUpdated();
-
 
     // Helper functions
-    void changeInputX(bool direction);
-    void updatePixelData();
+    void debug_updatePixelData();
 
     // Protected virtual functions for OpenGL, we do not call these
     protected:
     void initializeGL() override;
     void paintGL() override;
-
     void resizeGL(int w, int h) override;
-    //void paintEvent(QPaintEvent* event) override;
 };
 
 #endif // GLWIDGET_H
--- window.cpp
+++ window.cpp
@@ -4,16 +4,13 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <QPushButton>
-#include <QKeyEvent>
 
 Window::Window()
 {
     GLWidget* openGL = new GLWidget(this);
     QPushButton *button = new QPushButton(this);
     button->setText("Push me");
-    QWidget* layoutWidget = new QWidget(this);
     QGridLayout* layout = new QGridLayout(this);
-    //layoutWidget->setGeometry(20,20, 500, 300);
 
     QKeyEvent* event;
 
@@ -21,63 +18,4 @@ Window::Window()
     layout->addWidget(openGL, 0, 0);
     layout->addWidget(button, 0, 1);
     setLayout(layout);
-    QLabel* test_label = new QLabel(this);
-    test_label->setGeometry(50, 50, 500, 500);
-    if (openGL->test_W) {
-        test_label->setText("Pressed W");
-    }
-    else {
-        test_label->setText("No press W");
-    }
-
-
-    /*
-    if (event->key() == Qt::Key_F) {
-    }
-    */
-
 }
-
-void Window::keyPressEvent(QKeyEvent* event)
-{
-
-    if(event->key() == Qt::Key_W)
-    {
-    }
-
-    if(event->key() == Qt::Key_S)
-    {
-    }
-
-    if(event->key() == Qt::Key_A)
-    {
-    }
-
-    if(event->key() == Qt::Key_D)
-    {
-    }
-}
-
-/*
-void Window::keyPressEvent(QKeyEvent *event) {
-    switch (event->key()) {
-    case Qt::Key_W:
-        openGL->inputX = 256;
-        openGL->test_W = true;
-        this->update();
-        break;
-    case Qt::Key_S:
-        openGL->inputX = 0;
-        openGL->test_W = false;
-        this->update();
-        break;
-    case Qt::Key_Escape:
-        this->update();
-        break;
-    default:
-        event->ignore();
-        break;
-    }
-}
-
-*/
