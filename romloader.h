28c699126b5dd1b4f43c955f7bc3c118c655011a
Trost_linux <j.trosty@gmail.com>
2022-09-10 04:09:43 -0700

    Implemented a file loading system.

---
+++ romloader.h
@@ -0,0 +1,16 @@
+#ifndef ROMLOADER_H
+#define ROMLOADER_H
+
+#include <QFile>
+#include <QString>
+#include <QDebug>
+
+
+class RomLoader
+{
+public:
+    QByteArray nesTestRom;
+    RomLoader();
+};
+
+#endif // ROMLOADER_H
