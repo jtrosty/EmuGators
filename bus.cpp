c6f49f8c34cf7fbfe3a901f99e398a996c231749
Trost_linux <j.trosty@gmail.com>
2022-09-16 11:31:36 -0700

    Created ppu class. Filled out the basics of the new class, updated bus class to perform mirroring for ppu registers.

--- bus.cpp
+++ bus.cpp
@@ -18,16 +18,24 @@ void Bus::loadROM(QByteArray rom) {
     }
 }
 
-u8 Bus::cpuReadMemory(u16 addr) {
+u8 Bus::readMemory(u16 addr) {
+        // Ram Mirroring
     if ( addr < ramMirror) {
-        addr = addr & 0x1FFF;
+        addr = addr & ramMirror;
+    }
+        // PPU Mirroring
+    else if (addr >= ppuRegisterStart && addr <= ppuRegisterEnd) {
+        addr = addr & ppuMirror;
     }
     return memory[addr];
 }
 
-void Bus::cpuWriteMemory(u16 addr, u8 data) {
+void Bus::writeMemory(u16 addr, u8 data) {
     if ( addr < ramMirror) {
         addr = addr & 0x1FFF;
+    }
+    else if (addr >= ppuRegisterStart && addr <= ppuRegisterEnd) {
+        addr = addr & ppuMirror;
     }
     memory[addr] = data;
 }
