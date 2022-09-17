1fe5726ff8e45030239c4ad30cb59ad419aa058b
Matthew Graham <matthewjgraham@tutanota.com>
2022-09-17 19:11:12 -0400

    Fix silly mistake in the NESEmulator getters from previous commit

--- nesemulator.h
+++ nesemulator.h
@@ -10,8 +10,8 @@ public:
     static NESEmulator& the();
     void powerOn();
     CPU& cpu() { return mCPU; }
-    CPU const& cpu() const { return mBus; }
-    Bus& bus() { return mCPU; }
+    CPU const& cpu() const { return mCPU; }
+    Bus& bus() { return mBus; }
     Bus const& bus() const { return mBus; }
 private:
     NESEmulator();
