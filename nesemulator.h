9c4da187d57b393afd33f37aea115285273f9895
Matthew Graham <matthewjgraham@tutanota.com>
2022-09-17 19:09:57 -0400

    Add getters for the bus members in NESEmulator

--- nesemulator.h
+++ nesemulator.h
@@ -10,7 +10,9 @@ public:
     static NESEmulator& the();
     void powerOn();
     CPU& cpu() { return mCPU; }
-    CPU const& cpu() const { return mCPU; }
+    CPU const& cpu() const { return mBus; }
+    Bus& bus() { return mCPU; }
+    Bus const& bus() const { return mBus; }
 private:
     NESEmulator();
 
