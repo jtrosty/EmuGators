3ec2934f86d84803237ea2cfb04c6cf10c776201
Matthew Graham <matthewjgraham@tutanota.com>
2022-09-22 18:16:51 -0400

    Add a command line option to load a ROM in the future
    Does not actually change the rom to be loaded, but makes it easier
    down the line. It may not ever be used since the application will
    likely choose the roms through the GUI, but I like the idea of having
    options.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "window.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QApplication::setApplicationName("NES Emulator");
+    QApplication::setApplicationVersion("!Pre-alpha, if that!");
+    
+    QCommandLineParser parser;
+    parser.setApplicationDescription("NES Emulator");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("ROM", QApplication::translate("main", "ROM to load into the emulator"));
+
+    parser.process(a);
+    
+    const QStringList args = parser.positionalArguments();
+
+    if (args.length() == 0)
+	goto noROM; // only a temporary hack until we finish testing the Qt frontend
+
+noROM:
     Window w;
     w.show();
     return a.exec();
